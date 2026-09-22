045f2b396b8ce630615048a446b8a862fb03e379
slawekdk <s.ratynski.dk@gmail.com>
2026-09-22 12:09:33 +0200

    Add files via upload

--- app/static/sw.js
+++ app/static/sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'netflix-finder-static-v1';
+const CACHE = 'netflix-finder-static-v2';
 const STATIC = ['/','/static/index.html','/static/style.css','/static/app.js','/static/manifest.json','/static/icon-192.png','/static/icon-512.png','/static/icon-180.png'];
 self.addEventListener('install', event => { event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(STATIC)).then(()=>self.skipWaiting())); });
 self.addEventListener('activate', event => { event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))).then(()=>self.clients.claim())); });
